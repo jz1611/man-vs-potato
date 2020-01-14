ef8220a06c24864526bcd732586fe7bff23dd62b
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-01-14 14:18:04 -0700

    Password update works on backend

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   getUser: async (req, res, next) => {
     const db = req.app.get('db');
@@ -9,20 +11,39 @@ module.exports = {
   updateUser: async (req, res, next) => {
     const db = req.app.get('db');
     const { id } = req.session.user;
-    console.log(req.session.user)
     const { username, firstName, lastName, email, birthday, gender } = req.body;
     const foundUsername = await db.select_user_username(username);
     if (foundUsername.length) {
       if (foundUsername[0].user_id !== id) {
         res.status(409).send('There is already a user with that username.');
       } else {
-        await db.edit_user([id, username, firstName, lastName, email, birthday, gender]);
+        db.edit_user([id, username, firstName, lastName, email, birthday, gender]);
         req.session.user = {
           ...req.session.user,
           username: username
         };
         res.status(200).send('Successfully updated account.');
       }
+    } else {
+      db.edit_user([id, username, firstName, lastName, email, birthday, gender]);
+      req.session.user = {
+        ...req.session.user,
+        username: username
+      };
+      res.status(200).send('Successfully updated account.');
     }
+  },
+
+  updatePassword: async (req, res, next) => {
+    const db = req.app.get('db');
+    const { id } = req.session.user;
+    const { password } = req.body;
+    const saltRounds = 15;
+    bcrypt.genSalt(saltRounds).then(salt => {
+      bcrypt.hash(password, salt).then(hashedPassword => {
+        db.edit_password([id, hashedPassword]);
+        res.status(200).send('Password updated.');
+      });
+    });
   }
 }
