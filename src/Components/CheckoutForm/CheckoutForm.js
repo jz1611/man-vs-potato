4beac5c87f3d7385970380576398427184099822
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-01-29 10:38:26 -0700

    Fixing hosting.

--- src/Components/CheckoutForm/CheckoutForm.js
+++ src/Components/CheckoutForm/CheckoutForm.js
@@ -17,7 +17,9 @@ class CheckoutForm extends Component {
       body: token.id
     });
   
-    if (response.ok) console.log("Purchase Complete!")
+    if (response.ok) {
+      alert("Purchase Complete!");
+    }
   }
 
   render() {
